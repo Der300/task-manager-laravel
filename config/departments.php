0237be84273f5e973144f836465dd6a78f3d50d5
Tran Van Vu Luan <tranvanvuluantp@gmail.com>
2025-07-06 21:05:33 +0700

    GĐ 1-5: Cập nhật đường dẫn model User trong config/auth.php. Tạo mới config/positions.php và config/departments.php để lưu vị trí và bộ phận của người dùng. Với role client, hai cột này sẽ là null.

---
+++ config/departments.php
@@ -0,0 +1,10 @@
+<?php
+return[
+    'management'=> 'Management',
+    'project_mamagement'=> 'Project Management',
+    'frontend_team'=> 'Frontend Team',
+    'backend_team'=> 'Backend Team',
+    'design_team'=> 'Design Team',
+    'quality_assurance'=> 'Quality Assurance',
+    'business_analysis'=> 'Business Analysis'
+];
---
+++ config/positions.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'ceo' => 'Chief Executive Officer',
+    'secretary_to_ceo' => 'Secretary to CEO',
+    'admin' => 'Administration',
+    'project_manager' => 'Project Manager',
+    'leader' => 'Leader',
+    'fronend_dev' => 'Frontend Developer',
+    'backend_dev' => 'Backend Developer',
+    'uiux_designer' => 'UI/UX Designer',
+    'qa_engineer' => 'QA Engineer',
+    'devops_engineer' => 'DevOps Engineer',
+    'business_analyst' => 'Business Analyst'
+];
